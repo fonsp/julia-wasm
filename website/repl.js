ebfa222aeeb486e597999ade85a9a0706504371d
Fons van der Plas <fonsvdplas@gmail.com>
2021-02-25 12:38:26 +0100

    use pointers to objects and functions

--- website/repl.js
+++ website/repl.js
@@ -1,149 +1,7 @@
-/*
-    Network Protol
+const std = {}
 
-    This needs to match the message
-    types listed in ui/webserver/message_types.h.
-*/
-
-// input messages (to julia)
-var MSG_INPUT_NULL = 0
-var MSG_INPUT_START = 1
-var MSG_INPUT_POLL = 2
-var MSG_INPUT_EVAL = 3
-var MSG_INPUT_REPLAY_HISTORY = 4
-var MSG_INPUT_GET_USER = 5
-
-// output messages (to the browser)
-var MSG_OUTPUT_NULL = 0
-var MSG_OUTPUT_WELCOME = 1
-var MSG_OUTPUT_READY = 2
-var MSG_OUTPUT_MESSAGE = 3
-var MSG_OUTPUT_OTHER = 4
-var MSG_OUTPUT_EVAL_INPUT = 5
-var MSG_OUTPUT_FATAL_ERROR = 6
-var MSG_OUTPUT_EVAL_INCOMPLETE = 7
-var MSG_OUTPUT_EVAL_RESULT = 8
-var MSG_OUTPUT_EVAL_ERROR = 9
-var MSG_OUTPUT_PLOT = 10
-var MSG_OUTPUT_GET_USER = 11
-var MSG_OUTPUT_HTML = 12
-
-/*
-    REPL implementation.
-*/
-
-// the user name
-var user_name = "julia"
-
-// the user id
-var user_id = ""
-
-// indent string
-var indent_str = "    "
-
-// how long we delay in ms before polling the server again
-var poll_interval = 300
-
-// how long before we drop a request and try anew
-
-// keep track of whether we are waiting for a message (and don't send more if we are)
-var waiting_for_response = false
-
-// a queue of messages to be sent to the server
-var outbox_queue = []
-
-// a queue of messages from the server to be processed
-var inbox_queue = []
-
-// keep track of whether new terminal data will appear on a new line
-var new_line = true
-
-// keep track of whether we have received a fatal message
-var dead = false
-
-// keep track of terminal history
-var input_history = []
-var input_history_current = [""]
-var input_history_id = 0
-var input_history_size = 1000
-
-// an array of message handlers
-var message_handlers = []
-
-message_handlers[MSG_OUTPUT_NULL] = function (msg) {} // do nothing
-
-message_handlers[MSG_OUTPUT_READY] = function (msg) {
-    // // remove the initializing message
-    // $("#terminal").html("");
-    // // enable input
-    // $("#prompt").show();
-    // $("#terminal-input").removeAttr("disabled");
-    // $("#terminal-input").show();
-    // $("#terminal-input").focus();
-    // // reset the size of the input box
-    // set_input_width();
-}
-
-message_handlers[MSG_OUTPUT_MESSAGE] = function (msg) {
-    // print the message
-    // add_to_terminal("<span class=\"color-scheme-message\">"+escape_html(msg[0])+"</span><br /><br />");
-}
-
-message_handlers[MSG_OUTPUT_OTHER] = function (msg) {
-    // just print the output
-    // add_to_terminal(escape_html(msg[0]));
-}
-
-message_handlers[MSG_OUTPUT_FATAL_ERROR] = function (msg) {
-    // print the error message
-    // add_to_terminal("<span class=\"color-scheme-error\">"+escape_html(msg[0])+"</span><br /><br />");
-
-    // stop processing new messages
-    dead = true
-    inbox_queue = []
-    outbox_queue = []
-}
-
-message_handlers[MSG_OUTPUT_EVAL_INCOMPLETE] = function (msg) {
-    // // re-enable the input field
-    // $("#terminal-input").removeAttr("disabled");
-    // // focus the input field
-    // $("#terminal-input").focus();
-    // // add a newline for the user
-    // $("#terminal-input").newline_at_caret();
-}
-
-message_handlers[MSG_OUTPUT_EVAL_ERROR] = function (msg) {
-    // // print the error message
-    // add_to_terminal("<span class=\"color-scheme-error\">"+escape_html(msg[1])+"</span><br /><br />");
-    // // check if this was from us
-    // if (msg[0] == user_id) {
-    //     enable_prompt();
-    // }
-}
-
-message_handlers[MSG_OUTPUT_EVAL_RESULT] = function (msg) {
-    // // print the result
-    // if ($.trim(msg[1]) == "")
-    //     add_to_terminal("<br />");
-    // else
-    //     add_to_terminal(escape_html(msg[1])+"<br /><br />");
-    // if (msg[0] == user_id) {
-    //     enable_prompt();
-    // }
-}
-
-message_handlers[MSG_OUTPUT_HTML] = function (msg) {
-    // add_html_to_terminal(msg[1]);
-    // if (msg[0] == user_id) {
-    //     enable_prompt();
-    // }
-}
-
-function process_input(input) {
-    ptr = Module._malloc(input.length + 1)
-    Module.stringToUTF8(input, ptr, input.length + 1)
-    Module._jl_eval_and_print(ptr)
+const init_std = () => {
+    std["repr"] = register_jl_function(`x->(buf = IOBuffer(); show(buf, MIME"text/plain"(), x); String(take!(buf)))`)
 }
 
 var Module = {
@@ -183,6 +41,8 @@ var Module = {
             if (Module.initialize_jscall_runtime !== undefined) {
                 Module.initialize_jscall_runtime()
             }
+
+            init_std()
         },
     ],
 }
@@ -195,7 +55,48 @@ window.onerror = function (event) {
     }
 }
 
-var path = "https://keno.github.io/julia-wasm-build/hello-no-bysyncify.js"
 Module["locateFile"] = function (file, prefix) {
-    return path.substring(0, path.lastIndexOf("/")) + "/" + file
+    // var root = "https://fonsp-julia-wasm-build.netlify.app/"
+    var root = "https://keno.github.io/julia-wasm-build/"
+    return new URL(file, root).href
 }
+
+Module.JlProxy = {
+    isExtensible: function () {
+        return false
+    },
+    isJlProxy: function (jsobj) {
+        return jsobj["ptr"] !== undefined
+    },
+    has: function (jsobj, key) {
+        return false
+    },
+    getPtr: function (jsobj) {
+        return jsobj["ptr"]
+    },
+}
+
+///
+
+const eval_jl = (input) => {
+    ptr = Module._malloc(input.length + 1)
+    Module.stringToUTF8(input, ptr, input.length + 1)
+    result = Module._jl_eval_string(ptr)
+    return new Proxy({ ptr: result }, Module.JlProxy)
+}
+
+const register_jl_function = (code) => {
+    code_str_ptr = Module._malloc(code.length + 1)
+    Module.stringToUTF8(code, code_str_ptr, code.length + 1)
+    function_handle = Module._jl_eval_string(code_str_ptr)
+
+    return (arg) => {
+        arg_handle = Module.JlProxy.getPtr(arg)
+
+        str = Module._jl_call1(function_handle, arg_handle)
+        output = UTF8ToString(Module._jl_string_ptr(str), 4096)
+        return output
+    }
+}
+
+const process_input = (input) => std.repr(eval_jl(input))
